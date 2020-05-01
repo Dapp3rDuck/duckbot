d6a46e9e631be473729dd15427beb2f8594e2eff
Dapp3rDuck <mc.choof15@gmail.com>
2020-04-30 22:06:42 -0600

    yeet

--- bot.py
+++ bot.py
@@ -10,23 +10,6 @@ token = os.getenv("TOKEN")
 
 client = commands.Bot(command_prefix = 'd!')
 
-def isfloat(val):
-    try:
-        float(val)
-    except ValueError:
-        return False
-    return True
-
-def isint(val):
-    try:
-        int(val)
-    except ValueError:
-        return False
-    return True
-
-async def error(context, err):
-    await context.send(f"```ERROR: {err}```")
-
 @client.event
 async def on_ready():
     print('bot is ready')
@@ -47,24 +30,12 @@ async def ping(ctx):
 async def spam(ctx, *, text):
     msg = text.split(" ")
     cmd_args = []
-
-    if len(msg) > 2:
-        for x in range(2):
-            cmd_args.append(msg.pop(-1))
-            
-        if isfloat(cmd_args[0]) and isint(cmd_args[1]):
-            
-            delay = float(cmd_args[0])
-            repeat = int(cmd_args[1])
-
-            for x in range(repeat):
-                time.sleep(delay)
-                await ctx.send(' '.join(msg))
-    
-        else:
-            error(ctx, 'd!spam [message] [num of messages] [delay]')
-
-    else:
-        error(ctx, 'd!spam [message] [num of messages] [delay]')
+    for x in range(2):
+        cmd_args.append(msg.pop(-1))       
+    delay = float(cmd_args[0])
+    repeat = int(cmd_args[1])
+    for x in range(repeat):
+        await ctx.send(' '.join(msg))
+        time.sleep(delay)
 
 client.run(token)
