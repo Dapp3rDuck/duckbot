3873b0b8eb576adee7a5fbfb50a0da120d056634
Ryan Harrington <rharrington.web@gmail.com>
2020-05-01 16:50:31 -0600

    removed inline fields

--- bot.py
+++ bot.py
@@ -27,13 +27,9 @@ async def on_ready():
 @client.command()
 async def help(msg):
 
-<<<<<<< HEAD
-    info = "```----------- DuckBot Help ------------\nhttps://github.com/Dapp3rDuck/duckbot\n-------------------------------------\n"
-=======
     embed = discord.Embed(title="Help", description="Help info")
->>>>>>> 64bad58305bc61696e1a7207d365e1da828f60dc
     for command in helpInfo:
-        embed.add_field(name=command, value=helpInfo[command])
+        embed.add_field(name=command, value=helpInfo[command], inline=False)
 
     await msg.send(embed=embed)
 
