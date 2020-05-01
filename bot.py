1590c6eb04ec55702017b0b89b6551f46c6bae89
Dapp3rDuck <mc.choof15@gmail.com>
2020-04-30 20:08:35 -0600

    yeet

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import time
+import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -11,5 +13,21 @@ client = commands.Bot(command_prefix = 'd!')
 @client.event
 async def on_ready():
     print('bot is ready')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'{round(client.latency * 1000)}ms')
+
+@client.command()
+async def echo(ctx, *, text):
+    await ctx.send(text)
 
 client.run(token)
