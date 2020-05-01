6ed50fafac3d20989a498c5c907cec881af47728
Dapp3rDuck <mc.choof15@gmail.com>
2020-05-01 14:55:04 -0600

    insults

--- bot.py
+++ bot.py
@@ -7,23 +7,23 @@ from dotenv import load_dotenv
 
 dospam = False
 
+f = open("insults.txt", "r")
+insults = f.readlines()
+
 load_dotenv()
 token = os.getenv("TOKEN")
 
 client = commands.Bot(command_prefix = 'd!')
+client.remove_command('help')
 
 @client.event
 async def on_ready():
     print('bot is ready')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you beans"))
 
-@client.event
-async def on_member_join(member):
-    print(f'{member} has joined a server')
-
-@client.event
-async def on_member_remove(member):
-    print(f'{member} has left a server')
+@client.command()
+async def help(ctx):
+    await ctx.send("```DuckBot Help\n--}=========>\nhelp - does this\nping - shows your ping\nspam - spams a message\nstop - stops spamming, mabey```")
 
 @client.command()
 async def ping(ctx):
@@ -41,5 +41,9 @@ async def spam(ctx, *, text):
 async def stop(ctx):
     global dospam
     dospam = False
+
+@client.command()
+async def insult(ctx, *, text):
+    await ctx.send(text + " " + insults[random.randint(0,3)])
 
 client.run(token)
