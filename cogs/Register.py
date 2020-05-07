d59bef5017ff43b0d4dac3b03bf706f2d56d4c36
dapp3rsrv <mc.choof15@gmail.com>
2020-05-07 12:20:01 -0600

    [Server Sync]

--- cogs/Register.py
+++ cogs/Register.py
@@ -9,10 +9,12 @@ class Register(commands.Cog):
     # Register for Dapp3rCraft
     @commands.command()
     async def register(self, msg):
-        await msg.send('Registered')
+        user = msg.message.author
+        role = get(user.server.roles, name="Members")
+        await client.add_roles(user, role)
         channel = client.get_channel(707777532555952158)
         await channel.send('whitelist add ' + msg)
-        return
+        await msg.send('Registered ' + self + ' as ' + msg)
 
 def setup(client):
     client.add_cog(Register(client))
