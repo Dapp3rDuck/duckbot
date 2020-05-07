753c0da642be691fc615c0e3d95190b078256309
dapp3rsrv <mc.choof15@gmail.com>
2020-05-07 13:25:01 -0600

    [Server Sync]

--- cogs/Register.py
+++ cogs/Register.py
@@ -12,9 +12,9 @@ class Register(commands.Cog):
     async def register(self, msg):
         user = msg.message.author
         role = get(user.guild.roles, name="Members")
-        console = self.client.get_channel(707777532555952158)
         await user.add_roles(role)
-        await console.send('whitelist add ' + msg)
+        channel = self.client.get_channel(707777532555952158)
+        await channel.send('hello')
         await msg.send('Registered ' + self + ' as ' + msg)
 
 def setup(client):
