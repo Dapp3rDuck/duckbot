ca0c49c7cf12b9459f1157e837da00b963ede45c
Dapp3rDuck <mc.choof15@gmail.com>
2020-05-27 22:51:37 -0600

    BEANS!

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def on_ready():
     await client.change_presence(
         activity=discord.Activity(
             type=discord.ActivityType.watching, 
-            name="you beans"
+            name="you beans!"
         )
     )
 
